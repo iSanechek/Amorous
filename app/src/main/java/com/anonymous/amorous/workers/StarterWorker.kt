e59067524dfd8bb498047879f564da96d6a9bc89
isanechek <neizvestniytip@gmail.com>
2019-03-29 00:30:20 +0400

    rc StarterWorker

--- app/src/main/java/com/anonymous/amorous/workers/StarterWorker.kt
+++ app/src/main/java/com/anonymous/amorous/workers/StarterWorker.kt
@@ -1,11 +1,8 @@
 package com.anonymous.amorous.workers
 
 import android.content.Context
-import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.anonymous.amorous.utils.ConfigurationUtils
 import com.anonymous.amorous.utils.WorkersManager
-import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
 
 class StarterWorker(
@@ -16,23 +13,19 @@ class StarterWorker(
     private val manager: WorkersManager by inject()
 
     override suspend fun doWorkAsync(): Result {
-
-        if (configuration.getWorkerStatus()) {
-            sendEvent("StarterWorker", "Start workers!")
-            manager.startGeneralWorkers()
-        } else {
-            sendEvent("StarterWorker", "Stop all workers!")
-            manager.stopAllWorkers()
-        }
-
-        if (configuration.removeAllData()) {
-            sendEvent("StarterWorker", "Start  remove all data! :(")
+        sendEvent("StarterWorker", "Request workers!")
+        action.startAction {
+            sendEvent("StarterWorker", "Auth is done! Check status worker!")
             when {
-                fileUtils.clearCacheFolder(applicationContext) -> sendEvent("StarterWorker", "Clear cache folder done!")
-                fileUtils.getCacheFolderSize(applicationContext) == 0L -> sendEvent("StarterWorker", "Cache folder is empty!")
-                else -> sendEvent("StarterWorker", "Pizdos! I can't remove data from cache folder! :(")
+                configuration.getWorkerStatus() -> {
+                    sendEvent("StarterWorker", "Start workers!")
+                    manager.startGeneralWorkers()
+                }
+                else -> {
+                    sendEvent("StarterWorker", "Stop all workers!")
+                    manager.stopAllWorkers()
+                }
             }
-            database.clearDb()
         }
 
         sendEvents()
