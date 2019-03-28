dbff1616e41fa4b7d87ebf6fb2284a42f79597e9
isanechek <neizvestniytip@gmail.com>
2019-03-28 06:18:21 +0400

    update TrackingUtils

--- app/src/main/java/com/anonymous/amorous/utils/TrackingUtils.kt
+++ app/src/main/java/com/anonymous/amorous/utils/TrackingUtils.kt
@@ -2,16 +2,49 @@ package com.anonymous.amorous.utils
 
 import android.util.Log
 import com.anonymous.amorous.BuildConfig
+import com.anonymous.amorous.data.database.LocalDatabase
 import com.anonymous.amorous.data.models.Event
+import com.anonymous.amorous.data.models.Message
+import com.anonymous.amorous.empty
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 import java.util.*
 import kotlin.collections.HashSet
 
 interface TrackingUtils {
     fun sendEvent(tag: String, events: HashSet<String>)
+    fun sendEvent(tag: String, event: Event)
+    fun sendOnServer()
     fun log(msg: String?)
 }
 
-class TrackingUtilsImpl(private val remoteDatabase: RemoteDatabase) : TrackingUtils {
+class TrackingUtilsImpl(private val remoteDatabase: RemoteDatabase,
+                        private val database: LocalDatabase) : TrackingUtils {
+
+    override fun sendOnServer() {
+        try {
+            val events = database.getEvents()
+            if (events.isNotEmpty()) {
+                val jo = JSONObject()
+                val items = JSONArray()
+                for (event in events) {
+                    val item = JSONObject()
+                    item.put("id", event.id)
+                    item.put("title", event.title)
+                    item.put("date", event.date)
+                    item.put("event", event.event)
+                    items.put(item)
+                }
+                val msg = jo.put("events", items)
+                remoteDatabase.writeMessageInDatabase(Message("", msg.toString())) {
+                    database.clearEvents(events.map { it.id }.toSet())
+                }
+            } else addEvent("Events is empty!")
+        } catch (e: JSONException) {
+            addEvent(e.message ?: "Create message json error!")
+        }
+    }
 
     override fun log(msg: String?) {
         if (BuildConfig.DEBUG) {
@@ -19,19 +52,16 @@ class TrackingUtilsImpl(private val remoteDatabase: RemoteDatabase) : TrackingUt
         }
     }
 
-    override fun sendEvent(tag: String, events: HashSet<String>) {
-        for (e in events) {
-            log(e)
-        }
-        if (!BuildConfig.DEBUG) {
-            val builder = StringBuilder()
-            for (event in events) {
-                builder.append(event)
-                builder.append(",")
-            }
-            val event = Event(id = UUID.randomUUID().toString(), title = tag, date = System.currentTimeMillis(), event = builder.toString())
-            remoteDatabase.writeEventInDatabase(event)
+    override fun sendEvent(tag: String, event: Event) {
+        log(event.toString())
+        database.saveEvent(event)
+    }
 
-        }
+    override fun sendEvent(tag: String, events: HashSet<String>) {
+
+    }
+
+    private fun addEvent(msg: String) {
+        remoteDatabase.writeMessageInDatabase(Message(String.empty(), msg)) {}
     }
 }
